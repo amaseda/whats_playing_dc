0029d486dfcb06fde80d979740c95828ee2bec10
Jesse Shawl <jesse@jshawl.com>
2015-11-06 11:51:47 -0500

    add views

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require "sinatra/reloader"
 require_relative "models/next_week"
 
 get "/" do
-  monday = NextWeek.start.strftime("%Y-%m-%d")
-  sunday = NextWeek.end.strftime("%Y-%m-%d")
-  "The next closest Monday is #{monday}. The Sunday after that is #{sunday}"
+  @monday = NextWeek.start.strftime("%Y-%m-%d")
+  @sunday = NextWeek.end.strftime("%Y-%m-%d")
+  erb :index
 end
