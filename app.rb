771eae8fa33faff23813e59c8844d4db5cb12631
Jesse Shawl <jesse@jshawl.com>
2015-11-06 09:30:30 -0500

    add date calculator based on day

--- app.rb
+++ app.rb
@@ -1,6 +1,9 @@
 require "sinatra"
 require "sinatra/reloader"
+require_relative "models/next_week"
 
 get "/" do
-  "hello world"
+  monday = NextWeek.start.strftime("%Y-%m-%d")
+  sunday = NextWeek.end.strftime("%Y-%m-%d")
+  "The next closest Monday is #{monday}. The Sunday after that is #{sunday}"
 end
